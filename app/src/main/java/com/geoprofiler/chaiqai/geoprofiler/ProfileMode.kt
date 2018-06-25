e6fb70b1574b27ed8b500ac4427dd21fd69d1d21
Qaisar Mukhtar <36155259+Qaisarm@users.noreply.github.com>
2018-06-26 00:17:10 +0200

    added Home Screen, login and profile mode activity

---
+++ app/src/main/java/com/geoprofiler/chaiqai/geoprofiler/ProfileMode.kt
@@ -0,0 +1,12 @@
+package com.geoprofiler.chaiqai.geoprofiler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileMode : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_mode)
+    }
+}
