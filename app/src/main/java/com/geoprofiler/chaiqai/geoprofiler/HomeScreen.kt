861de26234205a07904d1a3914e5a2490f8a75d7
Qaisar Mukhtar <36155259+Qaisarm@users.noreply.github.com>
2018-06-27 01:11:13 +0200

    Modified Home Screen and created login and profiler mode activity

--- app/src/main/java/com/geoprofiler/chaiqai/geoprofiler/HomeScreen.kt
+++ app/src/main/java/com/geoprofiler/chaiqai/geoprofiler/HomeScreen.kt
@@ -1,12 +1,40 @@
 package com.geoprofiler.chaiqai.geoprofiler
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home_screen.*
+
 
 class HomeScreen : AppCompatActivity() {
+    // Home Button Items
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        login_btn.setOnClickListener()
+        {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+        help_btn.setOnClickListener(){
+            val intent = Intent (this, Help::class.java )
+            startActivity(intent)
+            finish()
+
+        }
+        setting_btn.setOnClickListener(){
+            val intent = Intent (this, Settings::class.java )
+            startActivity(intent)
+            finish()
+
+        }
     }
+
+
 }
